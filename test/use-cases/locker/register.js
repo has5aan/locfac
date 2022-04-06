78993613810ec1aa420b4dd06083048c1a0bbc18
has5aan <has5aan@outlook.com>
2022-04-06 12:45:49 +0200

    register locker use-case test

---
+++ test/use-cases/locker/register.js
@@ -0,0 +1,63 @@
+const chai = require("chai")
+chai.use(require("chai-as-promised"))
+const expect = chai.expect
+const sinon = require('sinon')
+const { BlockChainError, ValidationError } = require('../../../lib/errors')
+
+const makeRegister = require('../../../use-cases/locker/register')
+
+const repository = {
+    register: sinon.fake(() => new Promise((resolve, reject) => {
+        resolve()
+    }))
+}
+
+const repositoryFailing = {
+    register: sinon.fake(() => new Promise((resolve, reject) => {
+        reject()
+    }))
+}
+
+const validator = {
+    validate: sinon.fake(() => new Promise((resolve, reject) => {
+        resolve()
+    }))
+}
+
+const validatorFailing = {
+    validate: sinon.fake(() => new Promise((resolve, reject) => {
+        resolve()
+    }))
+}
+
+describe('Register locker use-case', async () => {
+
+    it('registers a locker', async () => {
+        const register = makeRegister({ repository, validator })
+
+        let args = { locker: 1, status: 1 }
+
+        await register(args)
+
+        expect(repository.register.calledOnceWith(args)).to.be.ok
+
+        args = {
+            data: { locker: 1, status: 1 },
+            rulesToBeValidated: ['locker', 'status']
+        }
+
+        expect(validator.validate.calledOnceWith(args)).to.be.ok
+    })
+
+    it('throws BlockChainError if repository throws an error', async () => {
+        const register = makeRegister({ repository: repositoryFailing, validator })
+
+        expect(register({ locker: 1, status: 1 })).to.be.rejectedWith(BlockChainError)
+    })
+
+    it('throws DomainError if repository throws an error', async () => {
+        const register = makeRegister({ repository, validator: validatorFailing })
+
+        expect(register({ locker: 1, status: 1 })).to.be.rejectedWith(ValidationError)
+    })
+})
