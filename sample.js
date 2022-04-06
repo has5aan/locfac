55607f43c5c71cc899486dd1cb75407556020f1a
has5aan <has5aan@outlook.com>
2022-04-06 16:45:44 +0200

    sample

--- sample.js
+++ sample.js
@@ -1,17 +1,8 @@
-const makeLockerRepository = require('./repositories/locker/locker')
-
-const web3 = require('./utils/web3')
-const contractDetails = require('./build/contracts/Locker.json')
-const env = require('./env/index.json')
 const accounts = require('./env/accounts.json')
 
-const contract = new web3.eth.Contract(contractDetails.abi, contractDetails.networks[env.network_id].address)
-contract.address = contract.options.address
-
-const postTransaction = require('./utils/postTransaction')
 const makeEthereumAccount = require('./utils/makeEthereumAccount')
 
-const makeLockerRepositoryFor = (poster) => makeLockerRepository({ contract, postTransaction, poster })
+const makeLockerRepositoryFor = require('./repositories/locker/lockerForPoster')
 
 const lockerRepository = makeLockerRepositoryFor(makeEthereumAccount(accounts[0].secretKey))
 
